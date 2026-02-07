5b0eac4902a08bfea84cfded2d91a9f4628a7e6a
Aljay Pascual <pascualaljay@gmail.com>
2026-02-08 05:57:57 +0800

    added to github

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nocoin/', // <--- ADD THIS LINE (Change 'nocoin' to your repo name)
 })
