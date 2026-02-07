1110bcd90425946d45f6393e4710770527125345
Aljay Pascual <pascualaljay@gmail.com>
2026-02-08 06:14:28 +0800

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nocoin/', // <--- ADD THIS LINE (Change 'nocoin' to your repo name)
+  base: '/', // <--- ADD THIS LINE (Change 'nocoin' to your repo name)
 })
